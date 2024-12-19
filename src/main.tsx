589d3dd745ef34e363a4b7c16d4d2be3fa82a199
gabbeeto <124828006+gabbeeto@users.noreply.github.com>
2024-12-19 09:54:42 -0300

    trying out other stuff

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,7 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client';
-import './index.css';
 
 createRoot(document.getElementById('root')!).render(
-<>
-<h2>hellooo!</h2>
-</>
+    <>
+      <h2>hellooo!</h2>
+    </>
 )
